6b7493d7fa213f79754744ad4e13780d7cb0a2f7
Alonso Parra <alonsogparra@gmail.com>
2022-11-18 16:48:04 -0400

    Route added mui-pagination

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CustomPagination } from './components/customPagination/CustomPagination';
 import { Dashboard } from './components/Dashboard';
+import { MuiPagination } from './components/muiPagination/MuiPagination';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/custom-pagination' element={<CustomPagination />} />
+        <Route path='/mui-pagination' element={<MuiPagination />} />
       </Routes>
     </Router>
   );
